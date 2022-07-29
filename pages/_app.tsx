143663b4d26650a3f41c34e7a4902e185218c6f9
inspira-mulher <inspiramulher2022@gmail.com>
2022-07-29 14:30:42 +0000

    Add facebook pixel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/main.css'
 
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    import('react-facebook-pixel')
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init('743606110228175')
+        ReactPixel.pageView()
+        router.events.on('routeChangeComplete', () => {
+          ReactPixel.pageView()
+        })
+      })
+  }, [router.events])
+  
   return <Component {...pageProps} />
 }
 
