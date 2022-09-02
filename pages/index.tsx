08f59e37f4b8e5bb3dc6f664a5147e8ebf44d7e3
inspira-mulher <inspiramulher2022@gmail.com>
2022-09-02 03:02:45 +0000

    change graphcms to local data

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,13 +10,12 @@ import Hero from '../components/hero'
 import Speakers from '../components/speakers'
 import Sponsors from '../components/sponsors'
 import Supporters from '../components/supporters'
+import { data } from '../data.json'
 import { Banner } from '../models/banner'
 import { Exhibitor } from '../models/exhibitor'
 import { Speaker } from '../models/speaker'
 import { Sponsor } from '../models/sponsor'
 import { Supporter } from '../models/supporter'
-
-const graphcms = new GraphQLClient('https://api-sa-east-1.graphcms.com/v2/cl47giwc63lq401z32ffkhc5r/master')
 
 const whatsappLink = 'https://api.whatsapp.com/send?phone=5549988851068&text=Ol%C3%A1!'
 
@@ -28,54 +27,8 @@ export type AppData = {
   supporters: Supporter[]
 }
 
-const QUERY = gql`
-  query {
-    banners {
-      id
-      image {
-        id
-        url
-      }
-    }
-    speakers {
-      id
-      title
-      description
-      image {
-        id
-        url
-      }
-    }
-    exhibitors {
-      id
-      link
-      image {
-        id
-        url
-      }
-    }
-    sponsors {
-      id
-      main
-      image {
-        id
-        url
-      }
-    }
-    supporters {
-      id
-      image {
-        id
-        url
-      }
-    }
-  }
-`
-
 export const getServerSideProps: GetServerSideProps<AppData> = async () => {
-  const props = await graphcms.request<AppData>(QUERY)
-  console.log(props)
-  return { props }
+  return { props: data }
 }
 
 const Home: NextPage<AppData> = ({ banners, speakers, exhibitors, sponsors, supporters }) => {
